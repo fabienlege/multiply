33b8e664766229ee2edfda72bfd8a6ab3b3f48e1
Fabien LEGE <fabien.lege@graneet.fr>
2025-01-20 01:12:26 +0100

    preselect all tables

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -20,7 +20,7 @@ export interface IAppState {
 
 const defaultAppState: IAppState = {
   screen: 'home',
-  tables: new Set<keyof typeof availableTables>(),
+  tables: new Set<keyof typeof availableTables>([2, 3, 4, 5, 6, 7, 8, 9]),
   timer: 0,
   turn: null,
   score: 0,

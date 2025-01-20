eb71254f0560f76aab9a379bde7902b6d8df3c14
Fabien LEGE <fabien.lege@graneet.fr>
2025-01-20 02:20:55 +0100

    allow submit with keyboard

--- src/screen/PlayScreen.tsx
+++ src/screen/PlayScreen.tsx
@@ -10,15 +10,16 @@ export const PlayScreen = React.memo(() => {
   const {selectedTable, selectedFactor} = turn!;
   const inputRef = React.useRef<HTMLInputElement>(null);
   return (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'stretch', justifyContent:'space-between', height:'100%', width:'100%'}}>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'stretch', justifyContent:'space-between', height:'100vh', width:'100%'}}>
       <Timer />
       <div>
         <h1>{selectedFactor}×{selectedTable}</h1>
       </div>
-      <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent:'center', gap:'1rem'}}>
-        <input type="number" style={{flex:1, height:'4rem', fontSize:'2rem'}} autoFocus ref={inputRef} />
-        <Button onClick={() => finishTurn(Number(inputRef.current?.value))}>Valider</Button>
+        <form style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent:'center', gap:'1rem'}} onSubmit={() => finishTurn(Number(inputRef.current?.value))}>
+        <input type="number" style={{flex:1, height:'4rem', fontSize:'2rem', width:'100px'}} autoFocus ref={inputRef} />
+        <Button type="submit">Valider</Button>
+        </form>
       </div>
-    </div>
+
     )
 });

b1f4412ebd661ff2ba977300282b40a81274c985
Fabien LEGE <fabien.lege@graneet.fr>
2026-01-18 18:13:28 +0100

    feat(table): add table of 25

--- src/constants/tables.ts
+++ src/constants/tables.ts
@@ -1,2 +1,2 @@
-export const availableTables = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+export const availableTables = [2, 3, 4, 5, 6, 7, 8, 9, 10, 25];
 export const availableFactor = [2, 3, 4, 5, 6, 7, 8, 9, 10];
